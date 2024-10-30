ebe84533f2735ce5e735d4dcf2a3cbda36196fac
Johan Sutrisno <johansutrisno30@gmail.com>
2024-10-30 23:04:57 +0700

    feat: add auth validation

---
+++ app/my-captions/page.tsx
@@ -0,0 +1,12 @@
+const MyCaptionsPage = () => {
+    return (<div className="flex items-center justify-center min-h-screen bg-gray-900">
+        <div className="text-center">
+            <h1 className="text-5xl font-bold text-white mb-4">Coming Soon</h1>
+            <p className="text-gray-300 text-lg">
+                We're working hard to bring you something amazing!
+            </p>
+        </div>
+    </div>)
+}
+
+export default MyCaptionsPage
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,8 +2,12 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { Button } from '@/components/ui/button'
 import { BookMarked } from 'lucide-react'
 import Link from 'next/link'
+import { useUser } from "@clerk/nextjs";
 
 export const Header = () => {
+    const { user } = useUser();
+    let href = user ? '/my-captions' : '/sign-in'
+
     return (
         <nav className="bg-background border-b">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -14,9 +18,11 @@ export const Header = () => {
                         </Link>
                     </div>
                     <div className="flex items-center space-x-4">
-                        <Button variant="outline">
-                            <BookMarked className="mr-2 h-4 w-4" /> My Captions
-                        </Button>
+                        <Link href={href}>
+                            <Button variant="outline">
+                                <BookMarked className="mr-2 h-4 w-4" /> My Captions
+                            </Button>
+                        </Link>
                         <SignedOut>
                             <SignInButton>
                                 <Button>Sign In</Button>
